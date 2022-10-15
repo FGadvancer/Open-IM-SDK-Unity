aa586f6028a711dacef487957a2724bd1b131fd2
yejian <596306006@qq.com>
2022-10-15 19:39:30 +0800

    first commit

---
+++ Assets/OpenIM/openim.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OpenIM;
+public class openim : MonoBehaviour
+{
+    public static string DEFAULT_IP = "121.37.25.71";
+//    public static final String DEFAULT_IP = "43.128.5.63";
+
+    //IM sdk api地址
+    public static string IM_API_URL = "http://" + DEFAULT_IP + ":10002";
+    //登录注册手机验 证服务器地址
+    public static string APP_AUTH_URL = "http://" + DEFAULT_IP + ":10004";
+    //web socket
+    public static string IM_WS_URL = "ws://" + DEFAULT_IP + ":10001";
+    // Start is called before the first frame update
+    void Start()
+    {
+    #if UNITY_ANDROID
+
+        OpenIMClient.Init();
+
+        InitInfo info = new InitInfo();
+        info.platform = 2;
+        info.api_addr = IM_API_URL;
+        info.ws_addr = IM_WS_URL;
+        info.data_dir = Application.persistentDataPath;
+        info.log_level = 1;
+        info.object_storage = "minio";
+        info.encryption_key = "";
+        OpenIMClient.initSDK(info);
+    #endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
---
+++ Assets/OpenIM/src/OpenIMClient.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+namespace OpenIM
+{
+    public class InitInfo{
+            public int platform;
+            public string api_addr;
+            public string ws_addr;
+            public string data_dir;
+            public int log_level;
+            public string object_storage;
+            public string encryption_key;
+    }
+
+    public partial class OpenIMClient
+    {
+        static AndroidJavaClass _Instance = null;
+
+               
+
+
+        public static void Init()
+        {
+            _Instance = new AndroidJavaClass("open_im_sdk.Open_im_sdk");
+            if (_Instance == null)
+                Debug.LogError("not find open_im_sdk.Open_im_sdk");
+
+            Debug.Log(_Instance);
+        }
+
+        public static void initSDK(InitInfo info)
+        {
+            if (_Instance == null) return;
+            var args = JsonUtility.ToJson(info);
+            Debug.Log("args = " + args);
+            _Instance.CallStatic<bool>("initSDK",new OnConnListenerCallBack(),Time.realtimeSinceStartup.ToString(),args);
+        }
+    }
+
+}
