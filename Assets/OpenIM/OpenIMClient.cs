d1f05e0dabbfb565611c256761284f93c7c6619a
Gordon <46924906+FGadvancer@users.noreply.github.com>
2023-08-31 21:01:55 +0800

    feat: lexternal c sharp interface of login

--- Assets/OpenIM/OpenIMClient.cs
+++ Assets/OpenIM/OpenIMClient.cs
@@ -4,25 +4,26 @@ using UnityEngine;
 using System;
 using System.Runtime.InteropServices;
 using System.Text;
-namespace OpenIM
+namespace OpenIMClient
 {
-    public delegate void CB();
-    public delegate void CB_I_S(int code, string msg);
-    public delegate void CB_S(string msg);
 
-    public class OpenIMClient
+public interface OnConnListener
+{
+    void OnConnecting();
+    void OnConnectSuccess();
+    void OnKickedOffline();
+    void OnUserTokenExpired();
+    void OnConnectFailed(int code, string msg);
+}
+
+    public partial class OpenIMClient
     {
-#if (UNITY_IPHONE || UNITY_TVOS || UNITY_WEBGL || UNITY_SWITCH) && !UNITY_EDITOR
-        const string OPENIMDLL = "__Internal";
-#else
-        const string OPENIMDLL = "openim";
-#endif
 
-        [DllImport(OPENIMDLL, CallingConvention = CallingConvention.Cdecl)]
-        public static extern int init_sdk(CB onConnecting, CB onConnectSuccess, CB onKickedOffline, CB onUserTokenExpired, CB_I_S onConnectFailed, string operationId, string config);
-
-        [DllImport(OPENIMDLL, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void login(CB_S onSuccess, CB_I_S OnFailed, string uid, string token);
+        public static  int InitSDK(OnConnListener callback, string operationId, string config)
+        {
+            return OpenIMClientProxy.init_sdk(callback.OnConnecting, callback.OnConnectSuccess,
+            callback.OnKickedOffline,callback.OnUserTokenExpired,callback.OnConnectFailed,operationId, config);
+        }
     }
 
 }
